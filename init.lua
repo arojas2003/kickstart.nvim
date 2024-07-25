8b206d00fe29e08c23512f147839e99f32969c8b
Anthony Rojas <anthony.rojas@utexas.edu>
2024-07-25 01:39:35 -0500

    Added Custom Remaps

--- init.lua
+++ init.lua
@@ -131,6 +131,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+------- ANTHONY'S CUSTOM REMAPS -------
+require 'custom.remap'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
