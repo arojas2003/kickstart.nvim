31eddf0bb11caee6f621db0575b9ca9f15c48c4c
Anthony Rojas <anthony.rojas@utexas.edu>
2024-07-25 02:05:31 -0500

    System Clipboard Remaps, Void Register deletion remap

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -6,3 +6,9 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Awesome: System Clipboard Yanking
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to System Clipboard' })
+
+-- Delete to void register (might conflict with [D]ocument)
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete to Void Register' })
