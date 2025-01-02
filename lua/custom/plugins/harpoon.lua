c0940061a6de3618cbcec39a47168f61228a95d7
Anthony Rojas <anthony.rojas@utexas.edu>
2025-01-02 03:02:22 -0600

    Fixed Harpoon Remap Performance Bug (Insert Mode "Space" Lag) * Previously made it so that you can replace buffer numbers in harpoon   in not only normal mode, but also insert and visual mode * Having a <leader> keymap in insert mode makes typing space lag hard   when typing. * Reverted back to the old keymap to remove lagging

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -22,7 +22,7 @@ return {
 
     -- Sets <leader> ALT-<num> to replace <num> in Harpoon
     local keymap_set_replace_nav = function(num)
-      vim.keymap.set({ 'n', 'i', 'v' }, string.format('<leader><A-%d>', num), function()
+      vim.keymap.set('n', string.format('<leader><A-%d>', num), function()
         harpoon:list():replace_at(num)
       end, { desc = string.format('Harpoon: Replace file %d', num) })
     end
