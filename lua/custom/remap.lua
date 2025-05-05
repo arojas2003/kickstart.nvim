45219a71d03404b5b0b267752cb77ff1d6931dc0
Anthony Rojas <anthony.rojas@utexas.edu>
2025-05-05 01:17:08 +0000

    Added Quickfix List and Location List keybindings

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -12,3 +12,11 @@ vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to System Clip
 
 -- Delete to void register (might conflict with [D]ocument)
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete to Void Register' })
+
+-- Quick Fix List stuff
+vim.keymap.set('n', '<A-k>', '<cmd>cnext<CR>zz', { desc = 'Jump to next item in Quickfix List' })
+vim.keymap.set('n', '<A-j>', '<cmd>cprev<CR>zz', { desc = 'Jump to previous item in Quickfix List' })
+
+-- Location List stuff
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz', { desc = 'Jump to next item in Location List' })
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz', { desc = 'Jump to previous item in Location List' })
