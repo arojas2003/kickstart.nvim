3b725b073dd405faf8b4474c7177cb99a49dd129
arojas2003 <anthony.rojas@utexas.edu>
2026-08-07 13:46:30 -0700

    Changed Gitsigns to look like VSCode

--- init.lua
+++ init.lua
@@ -364,13 +364,13 @@ do
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   vim.pack.add { gh 'lewis6991/gitsigns.nvim' }
   require('gitsigns').setup {
-    signs = {
-      add = { text = '+' }, ---@diagnostic disable-line: missing-fields
-      change = { text = '~' }, ---@diagnostic disable-line: missing-fields
-      delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
-      topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
-      changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
-    },
+    -- signs = {
+    --   add = { text = '+' }, ---@diagnostic disable-line: missing-fields
+    --   change = { text = '~' }, ---@diagnostic disable-line: missing-fields
+    --   delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
+    --   topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
+    --   changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
+    -- },
   }
 
   -- Useful plugin to show you pending keybinds.
