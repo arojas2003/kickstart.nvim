23b815e2fafeee10e443d7f1c166a0ab4c19d27d
arojas2003 <anthony.rojas@utexas.edu>
2026-08-07 14:03:47 -0700

    Added Yanking Remaps (system clipboard & void register deletion)

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -6,3 +6,9 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- System Clipboard Yanking
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to System Clipboard' })
+
+-- Delete to void register
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete to Void Register' })
