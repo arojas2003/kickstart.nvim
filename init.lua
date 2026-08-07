2f4a03d510fb169a1cb23622aff8c86f365ed827
arojas2003 <anthony.rojas@utexas.edu>
2026-08-07 11:58:31 -0700

    Force Tabs to be 4 spaces

--- init.lua
+++ init.lua
@@ -167,6 +167,12 @@ do
   -- Minimal number of screen lines to keep above and below the cursor.
   vim.o.scrolloff = 0
 
+  -- Force tabs to be 4 spaces
+  vim.o.tabstop = 4
+  vim.o.softtabstop = 4
+  vim.o.shiftwidth = 4
+  vim.o.expandtab = true
+
   -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
