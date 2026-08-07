05cb714a6996afe5f090770fb9c9bd2b222b8eac
arojas2003 <anthony.rojas@utexas.edu>
2026-08-07 16:01:32 -0700

    Added Extra Configuration for Java Projects
    * Added jdtls extension
    * Made it so that jdtls and gradle_ls are installed on Mason
    * Added ftplugin/java.lua for extra configuration details

--- init.lua
+++ init.lua
@@ -701,6 +701,9 @@ do
     -- But for many setups, the LSP (`ts_ls`) will work just fine
     -- ts_ls = {},
 
+    jdtls = {},
+    gradle_ls = {},
+
     stylua = {}, -- Used to format Lua code
 
     -- Special Lua Config, as recommended by neovim help docs
